c28236155fee9d7bf29d337722aef1f780487bca
JohnAlbrektsson <114058148+JohnAlbrektsson@users.noreply.github.com>
2023-04-30 13:06:21 +0200

    Add project files.

---
+++ AvanceradDotNet-Labb3/Car.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvanceradDotNet_Labb3
+{
+    internal class Car
+    {
+        public string Name { get; set; }
+
+        public int Speed { get; set; }
+        public double Distance { get; set; }
+
+        public static string Winner;
+
+        public Car(string name)
+        {
+            Name = name;
+            Speed = 120;
+            Distance = 0;
+        }
+        public void Race()
+        {
+            Console.WriteLine($"{Name} har startat");
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            while (Distance <= 10000)
+            {
+                Distance += Speed/3.6;
+                Thread.Sleep(1000);
+                if (sw.Elapsed.TotalSeconds > 29)
+                {
+                    TimedEvent();
+                    sw.Restart();
+                }
+            }
+            Console.WriteLine($"{Name} har nått målet");
+            if (Winner == null)
+            {
+                Winner = Name;
+                Console.WriteLine($"{Name} vann racet");
+            }
+        }
+        public  void Refuel()
+        {
+            Console.WriteLine($"{Name} behöver tanka stannar 30 sek");
+            Thread.Sleep(30000);
+        }
+        public  void BirdonWindow()
+        {
+            Console.WriteLine($"{Name} har en fågel på vindrutan, stannar 10 sek");
+            Thread.Sleep(10000);
+        }
+        public  void EngineFailure()
+        {
+            Console.WriteLine($"{Name} har fått motorfel och kör nu {Speed}km/h");
+            Speed -= 1; 
+        }
+        public  void Puncture()
+        {
+            Console.WriteLine($"{Name} har fått punktering, stannar 20 sek");
+            Thread.Sleep(20000);
+        }
+        public void TimedEvent()
+        {
+
+            Random random = new Random();
+            int rn = random.Next(0, 50);
+            if (rn == 1)
+            {
+                Refuel();
+            }
+            else if (rn > 1 && rn < 4)
+            {
+                Puncture();
+            }
+            else if (rn > 3 && rn < 9)
+            {
+                BirdonWindow();
+            }
+            else if (rn > 8 && rn < 19)
+            {
+                EngineFailure();
+            }
+        }
+    }
+}
---
+++ AvanceradDotNet-Labb3/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace AvanceradDotNet_Labb3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car("BMW");
+            Car car2 = new Car("Toyota");
+            Car car3 = new Car("Audi");
+
+            Thread thread1 = new Thread(()=>car1.Race());
+            Thread thread2 = new Thread(()=>car2.Race());
+            Thread thread3 = new Thread(()=>car3.Race());
+
+            thread1.Start();
+            thread2.Start();
+            thread3.Start();
+
+            Console.WriteLine("Racet startar");
+
+            Thread thread4 = new Thread(()=>Status(car1,car2,car3));
+            thread4.Start();
+
+            thread1.Join();
+            thread2.Join();
+            thread3.Join();
+        }
+        public static void Status(Car car1,Car car2,Car car3)
+        {
+            while (true)
+            {
+                if (Console.ReadLine().ToLower() == "status")
+                {
+                    Console.WriteLine($"{car1.Name} har kommit {(int)car1.Distance}m och har en hastighet på {car1.Speed}km/h");
+                    Console.WriteLine($"{car2.Name} har kommit {(int)car2.Distance}m och har en hastighet på {car2.Speed}km/h");
+                    Console.WriteLine($"{car3.Name} har kommit {(int)car3.Distance}m och har en hastighet på {car3.Speed}km/h");
+                }
+            }
+        }
+    }
+}
